399f6d2d3acd188eaac29da2ceb5467faa69028c
Your Name <you@example.com>
2014-03-31 16:03:11 +0200

    more rocket science

--- cp210x_gpio_on.c
+++ cp210x_gpio_on.c
@@ -4,46 +4,51 @@
 #include <termios.h>
 #include <stdio.h>
 
-#define IOCTL_GPIOGET 0x8000
-#define IOCTL_GPIOSET 0x8001
+#define IOCTL_GPIOGET	0x8000
+#define IOCTL_GPIOSET	0x8001
+#define GPIO_0_ON	0x00010001
+#define GPIO_0_OFF	0x00000001
+#define GPIO_1_ON	GPIO_0_ON  << 1
+#define GPIO_1_OFF	GPIO_0_OFF << 1
+#define GPIO_2_ON	GPIO_0_ON  << 2
+#define GPIO_2_OFF	GPIO_0_OFF << 2
+#define GPIO_3_ON	GPIO_0_ON  << 3
+#define GPIO_3_OFF	GPIO_0_OFF << 3
 
-int main()
-{
+int main() {
 	int fd;
-	printf("CP2104 GPIO Test\n");
+	unsigned int gpio, i;
+	printf("CP2104 GPIO Test on ttyUSB0\n");
 	fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
-	if (fd == -1)
-	{
+	if (fd == -1) {
 		fprintf(stderr,"Error opening port /dev/ttyUSB0\n");
 		return -1;
 	}
-
-	unsigned gpio, i;
 	gpio = 0;
 
 	ioctl(fd, IOCTL_GPIOGET, &gpio);
 	printf("original gpio = 0x%08X\n",gpio);
 
 	for (i = 0; i<= 10000; i++) {
-		gpio = 0x00010001;
+		/* switch on the GPIOs */
+		gpio = GPIO_0_ON;
 		ioctl(fd, IOCTL_GPIOSET, &gpio);
-		gpio = 0x00020002;
+		gpio = GPIO_1_ON;
 		ioctl(fd, IOCTL_GPIOSET, &gpio);
-		gpio = 0x00040004;
+		gpio = GPIO_2_ON;
 		ioctl(fd, IOCTL_GPIOSET, &gpio);
-		gpio = 0x00080008;
+		gpio = GPIO_3_ON;
 		ioctl(fd, IOCTL_GPIOSET, &gpio);
-		gpio = 0x00000008;
+		/* switch off the GPIOs - reverse */
+		gpio = GPIO_3_OFF;
 		ioctl(fd, IOCTL_GPIOSET, &gpio);
-		gpio = 0x00000004;
+		gpio = GPIO_2_OFF;
 		ioctl(fd, IOCTL_GPIOSET, &gpio);
-		gpio = 0x00000002;
+		gpio = GPIO_1_OFF;
 		ioctl(fd, IOCTL_GPIOSET, &gpio);
-		gpio = 0x00000001;
+		gpio = GPIO_0_OFF;
 		ioctl(fd, IOCTL_GPIOSET, &gpio);
 	}
-
 	close(fd);
-
 	return 0;
 }
